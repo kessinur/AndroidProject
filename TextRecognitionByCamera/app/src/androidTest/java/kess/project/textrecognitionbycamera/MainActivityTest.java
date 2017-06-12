b388e34e9ac086e7e805ad6979bf3359c75ad7a1
kessinur <kessi8243@students.amikom.ac.id>
2017-06-13 00:02:50 +0800

    Initial commit

---
+++ TextRecognitionByCamera/app/src/androidTest/java/kess/project/textrecognitionbycamera/MainActivityTest.java
@@ -0,0 +1,10 @@
+package android.project.textrecognitionbycamera;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Kessi Nur Wibowo on 12/06/2017.
+ */
+public class MainActivityTest {
+
+}
